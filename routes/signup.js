a5fd7eec58f50107668dcc93153006ad48963ca5
lexlevi17 <sphota9@gmail.com>
2017-01-29 02:28:25 -0500

    refactor register endpoint

--- routes/signup.js
+++ routes/signup.js
@@ -6,31 +6,20 @@ var router = express.Router();
 var db = require('../db/db');
 
 router.post('/',function(req, res){
-    var user_name = req.body.user;
-    var password = req.body.password;
-    var first_name = req.body.firstName;
-    var last_name = req.body.lastName;
-    var email = req.body.email;
-    var courses = req.body.courses;
-    var major = req.body.major;
-    var times = req.body.times;
-    var year = req.body.year;
-    var contrib = req.body.contribution;
-    console.log("New User:");
-    console.log("User name = " + email + ", password is " + password);
     var user = new db.User();
-    user.userName = user_name;
-    user.password = password;
-    user.firstName = first_name;
-    user.lastName = last_name;
-    user.email = email;
-    user.courses = courses;
-    user.major = major;
-    user.times = times;
-    user.year = year;
-    user.contrib = contrib;
+    user.userName = req.body.user;
+    user.password = req.body.password;
+    user.firstName = req.body.firstName;
+    user.lastName = req.body.lastName;
+    user.email = req.body.email;
+    user.courses = req.body.courses;
+    user.major = req.body.major;
+    user.times = req.body.times;
+    user.year = req.body.year;
+    user.contrib = req.body.contribution;
     user.save();
-
+    console.log("New User:");
+    console.log("User name = " + user.email + ", password is " + user.password);
     res.send(user);
 });
 
