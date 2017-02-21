ab185b4ea4737a2d31290567026837e7aa958fd4
lexlevi17 <sphota9@gmail.com>
2017-02-20 21:22:23 -0500

    add chat endpoints

---
+++ routes/chats.js
@@ -0,0 +1,97 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../db/db');
+var constants = require("../constants");
+var ObjectId = require('mongodb').ObjectId;
+
+// GET Chats for Group
+router.get('/:groupId', function (req, res) {
+    db.Chat.find({ 'group': ObjectId(req.params.groupId) }, '_id', function (err, chats) {
+        if (err || chats === null || chats.length === 0) {
+            res.status(400).send({ error: "No Chats in Group" });
+        }
+        // array to hold conversations + most recent messages
+        var fullChats = [];
+        chats.forEach(function(chat) {
+            db.Message.find({ 'chat': chat.id })
+                .sort('-createdAt')
+                .limit(1)
+                .populate({
+                    path: 'author',
+                    select: "firstName lastName userName"
+                })
+                .exec(function (err, message) {
+                    if (err)
+                        res.status(400).send({ error: "No Messages in Chat" });
+                    else {
+                        fullChats.push(message);
+                        if (fullChats.length === chats.length) {
+                            return res.status(200).json({ conversations: fullChats });
+                        }
+                    }
+                })
+        })
+
+    });
+});
+
+// GET Messages for Chat
+router.get('/:id/messages', function (req, res) {
+    db.Message.find({ 'chat': req.params.id }, 'createdAt body author')
+        .sort('-createdAt')
+        .populate({
+            path: 'author',
+            select: 'firstName lastName userName'
+        }).exec(function (err, messages) {
+            if (err || messages === null || messages.length === 0)
+                res.status(404).send({ error: "Not Messages Found for Channel" });
+            else
+                res.status(200).send(messages);
+    });
+});
+
+// POST new Chat in Group
+router.post('/:groupId', function (req, res) {
+    if (!req.body.user)
+        res.status(422).send({ error: 'Please choose a valid recipient for your message.' });
+    if (!req.body.message)
+        res.status(422).send({ error: 'Please enter a message.' });
+    var chat = new db.Chat();
+    chat.participants = [req.body.user.id];
+    for (p in req.body.participants) {
+        chat.participants.push(ObjectId(p.id));
+    }
+    chat.isPrivate = req.body.isPrivate;
+    chat.group = ObjectId(req.params.id);
+    chat.save(function (err, newChat) {
+        if (err)
+            res.send({ error: err });
+        var message = new db.Message();
+        message.chat = newChat.id;
+        messaeg.body = req.body.message.body;
+        message.author = req.body.message.author;
+        message.save(function(err, newMessage) {
+            if (err)
+                res.send({ error: err });
+            else
+                res.status(200).send(chat);
+        });
+
+    })
+});
+
+// POST new Message to Chat
+router.post('/:id/message/new', function (req, res) {
+    var reply = new db.Message();
+    reply.chat = ObjectId(req.params.id);
+    reply.body = req.body.body;
+    reply.author = req.body.author;
+    reply.save(function(err, sentReply) {
+        if (err)
+            res.send({ error: err });
+        else
+            res.status(200).send(sentReply);
+    });
+});
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -4,31 +4,33 @@ var db = require('../db/db');
 var bcrypt = require('bcrypt');
 var ObjectId = require('mongodb').ObjectId;
 
-// GET Users listing
+// GET all Users
 router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+    db.User.find(function (err, users) {
+        if (err || users === null)
+            res.status(404).send({ error: "No users found" });
+        else
+            res.send(users);
+    });
 });
 
-// GET Groups for user
+// GET Groups for User
 router.get('/:id/groups', function(req, res, next) {
-    db.User.findOne( { '_id': ObjectId(res.params.id) }, function (err, user) {
-        if (err || user === null) {
-            res.status(404);
-            res.send({ "error": "User not found" });
-        } else {
+    db.User.findOne( { '_id': ObjectId(req.params.id) }, function (err, user) {
+        if (err || user === null)
+            res.status(404).send({ error: "User not found" });
+        else {
             db.Group.find({
                 '_id': { $in: user.groups }
             }, function (err, groups) {
-                if (err || groups === null || groups.empty()) {
-                    res.status(404);
-                    res.send([]);
+                if (err || groups === null || groups.length === 0) {
+                    res.status(404).send({ error: "User has no Groups" });
+                } else {
+                    res.status(200).send(groups);
                 }
-                res.status(200);
-                res.send(groups);
             });
         }
     });
-    res.send('respond with a resource');
 });
 
 module.exports = router;
