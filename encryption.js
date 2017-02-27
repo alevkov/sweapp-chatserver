833a99c5b964e165142257e36219ac815270e73a
Joanna Bitton <nokogiri@Joannas-MacBook-Pro.local>
2017-02-27 01:53:21 -0500

    change salt

--- encryption.js
+++ encryption.js
@@ -1,17 +1,17 @@
 var crypto = require('crypto'),
     algorithm = 'aes-256-ctr',
-    password = 'd6F3Efeq';
+    salt = 'fifeh^&$9437487542';
 
 module.exports = {
     encrypt: function (text) {
-        var cipher = crypto.createCipher(algorithm,password);
+        var cipher = crypto.createCipher(algorithm,salt);
         var crypted = cipher.update(text,'utf8','hex');
         crypted += cipher.final('hex');
         return crypted;
     },
 
     decrypt: function (text) {
-        var decipher = crypto.createDecipher(algorithm,password);
+        var decipher = crypto.createDecipher(algorithm,salt);
         var dec = decipher.update(text,'hex','utf8');
         dec += decipher.final('utf8');
         return dec;
