a324a019105324108f6e540e4f589f172a183c29
lexlevi17 <sphota9@gmail.com>
2017-03-08 15:22:50 -0500

    add send message socket event

--- sockets.js
+++ sockets.js
@@ -1,25 +1,41 @@
 var socketio = require('socket.io');
 var constants = require('./constants');
+var db = require('./db/db');
+var ObjectId = require('mongodb').ObjectId;
+var encryption = require("./encryption");
+
 
 // universal listen goes to www
 module.exports.listen = function (app) {
     io = socketio(app);
     io.on(constants.ioConnection, function (socket) {
-        console.log("Someone connected");
+        console.log(socket.id + " connected");
         socket.on(constants.ioEnterChannel, function (channel) {
+            console.log(socket.id + " joined channel " + channel);
             socket.join(channel);
         });
         socket.on(constants.ioLeaveChannel, function (channel) {
+            console.log(socket.id + " left channel " + channel);
             socket.leave(channel);
         });
-        socket.on(constants.ioNewMessages, function (channel) {
-            io.sockets.in(channel).emit(constants.ioRefreshMessages, channel);
+        socket.on(constants.ioNewMessage, function (channel, message) {
+            var m = JSON.parse(message);
+            var reply = new db.Message();
+            reply.chat = ObjectId(m.chat);
+            reply.body = encryption.encrypt(m.body);
+            reply.author = m.author._id;
+            reply.save(function(err, sentReply) {
+                if (err)
+                    res.send({ error: err });
+                else
+                    io.sockets.in(channel).emit(constants.ioNewMessage, message);
+            });
         });
         socket.on(constants.ioDisconnect, function () {
-            console.log("Someone disconnected");
+            console.log(socket.id + " disconnected");
         });
         socket.on(constants.ioTyping, function (user) {
-            console.log("Someone is typing");
+            console.log(socket.id + "is typing");
         })
     });
 
